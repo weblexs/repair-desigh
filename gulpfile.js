a89a561936b6ae73eb77a5aedc2c1f87f191e583
Aibolitik <samsungS6z@yandex.ru>
2020-05-10 05:09:07 +0500

    feat: add Futura Book & Demi fonts

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,25 @@ const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
-
 // Static server
 function bs() {
-  serveSass();
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
-  watch("./*.html").on('change', browserSync.reload);
-  watch("./sass/**/*.sass", serveSass);
-  watch("./js/*.js").on('change', browserSync.reload);
+    serveSass();
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    watch("./*.html").on('change', browserSync.reload);
+    watch("./sass/**/*.sass", serveSass);
+    watch("./sass/**/*.scss", serveSass);
+    watch("./js/*.js").on('change', browserSync.reload);
 };
 
 function serveSass() {
-  return src("./sass/*.sass")
-    .pipe(sass())
-    .pipe(autoprefixer({
-      cascade: false
-  }))
-    .pipe(dest("./css"))
-    .pipe(browserSync.stream());
+    return src("./sass/**/*.sass", "./sass/**/*.sass)
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
 };
 
 exports.serve = bs;
