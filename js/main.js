d400fe3352b1b91ae15c95b0403f87ba83285c92
Aibolitik <samsungS6z@yandex.ru>
2020-05-20 22:30:46 +0500

    feat: Connecting and configuring the slider

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-/* document.addEventListener("DOMContentLoaded", function(event) {
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
-  const closeBtn = document.querySelector('.modal__close');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  }
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
-});
-
-closeBtn.addEventListener('click', switchModal);
-
-}); */
-
-
-
 $(document).ready(function () {
   var modal = $('.modal'),
       modalBtn = $('[data-toggle="modal"]'),
@@ -26,4 +9,27 @@ $(document).ready(function () {
   closeBtn.on('click', function () {
     modal.toggleClass('modal--visible');
   });
+
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+/*   next.css('left', prev.widht() ) */
+
+  next.css('left', prev.widht() + 10 + bullets.widht() + 10)
+  bullets.css('left', prev.widht() + 10)
+
 });
+
